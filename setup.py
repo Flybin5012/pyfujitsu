e2cd9acc09df237c9636859cc7e6ec2ac68adf8a
Flybin5012 <42439359+Flybin5012@users.noreply.github.com>
2022-04-10 17:10:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfujitsu",
-    version="0.9.19",
+    version="0.9.20",
     author="Mehdi Modarressi",
     author_email="Luckposht@gmail.com",
     description="Fujitsu Airconditioners",
     long_description="Python library to control Fujitsu General Airconditioners on AylaNetworks IoT platform",
-    url="https://github.com/xerxes87/pyfujitseu",
+    url="https://github.com/Flybin5012/pyfujitsu",
     packages=setuptools.find_packages(),
     install_requires=['requests','certifi','chardet','idna','urllib3'] ,
     classifiers=[
